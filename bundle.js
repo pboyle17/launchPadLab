84e45a8943d42f73f887b748a871e8b91cbbec5b
Paul Boyle <pboyle17@gmail.com>
2016-09-05 19:38:29 -0500

    resized all images to 200x200

--- bundle.js
+++ bundle.js
@@ -78,6 +78,8 @@ let logo = {
     let $modal = document.querySelector('#modal');
     $imgElement.src = obj.logo;
     $imgElement.id = obj.name;
+    $imgElement.style.height = '200px';
+    $imgElement.style.width = '200px';
     $imgElement.addEventListener('click',()=>{
       modal.toggleModal(obj);
     });
